da16fd2bc653e0a472d2d534da0d9c0b4f10e073
Brijeshlakkad <brijeshlakkad22@gmail.com>
2018-03-01 16:09:13 +0530

    Added functions for tracking of progress and status bit

--- candidate_details.php
+++ candidate_details.php
@@ -8,11 +8,20 @@ $login_email="";
 $login_name="";
 $login_mno="";
 $login_image="";
-function get_details_from_candidate()
-{
-if(isset($_SESSION['Userid']))
+$row="";
+function connect_with_row()
 {
 	global $con;
+	global $row;
+	$user_check=$_SESSION['Userid'];
+	$r="select * from Candidates where Email='$user_check' ";
+	$ses_sql=mysqli_query($con,$r);
+	$row=mysqli_fetch_assoc($ses_sql);
+}
+function get_details_from_candidate()
+{
+	connect_with_row();
+	global $row;
 	global $login_id;
 	global $login_name;
 	global $login_email;
@@ -23,22 +32,74 @@ if(isset($_SESSION['Userid']))
 	global $barV;
 	global $squali;
 	global $qualis;
-	$user_check=$_SESSION['Userid'];
-	$r="select * from Candidates where Email='$user_check' ";
-	$ses_sql=mysqli_query($con,$r);
-	$row=mysqli_fetch_assoc($ses_sql);
+	global $course;
+	global $college;
+	global $p_year;
+	global $intern;
+	
 	$login_id=$row['ID'];
 	$login_name=$row['Name'];
 	$login_email=$row['Email'];
 	$login_mno=$row['Phone'];
 	$login_image=$row['Image'];
+	
 	$sbit=$row['Status_bits'];
 	$bits=explode(",/,",$sbit);
 	
 	$barV=$row['Progress'];
+	
 	$squali=$row['Quali'];
 	$qualis=explode(",/,",$squali);
+	
+	$course=$row['Course'];
+	$p_year=$row['Passing_year'];
+	$intern=$row['Intern'];
+	$college=$row["College"];
 }
+function set_progress($f)
+{
+	global $course;
+	global $qualis;
+	global $barV;
+	get_details_from_candidate();
+	if($f=="skills")
+	{
+		if($qualis[0]=='0')
+		{
+				$barV+=15;
+		}
+		return $barV;
+	}
+	else if($f=="gra")
+	{
+		if($course == '-99')
+		{
+				$barV+=15;
+		}
+		return $barV;
+	}
 }
-
+function set_bits($f)
+{
+	global $course;
+	global $qualis;
+	global $bits;
+	get_details_from_candidate();
+	if($f=="skills")
+	{
+		if($qualis[0]=='0')
+		{
+			$bits[0]+=1;
+		}
+		return implode(",/,",$bits);
+	}
+	else if($f=="gra")
+	{
+		if($course=='-99')
+		{
+			$bits[0]+=1;
+		}
+		return implode(",/,",$bits);
+	}
+}
 ?>
