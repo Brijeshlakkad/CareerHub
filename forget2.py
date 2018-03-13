dde7e8d04d24764f7aef2f717980afdd14df286d
Brijeshlakkad <brijeshlakkad22@gmail.com>
2018-03-13 14:37:17 +0530

    Fixed all bugs in forget password 'don't forget to add your email and password to send email from'

---
+++ forget2.py
@@ -0,0 +1,46 @@
+#!C:\Users\RAJ\AppData\Local\Programs\Python\Python36\python
+import sys
+import os
+import re
+from smtplib import SMTP_SSL as SMTP 
+from email.mime.text import MIMEText
+import cgi, cgitb 
+form = cgi.FieldStorage() 
+ 
+print("Content-type:text/html\r\n\r\n")
+
+if (form.getvalue('forget_email')):
+	if form.getvalue('otp'):
+		forget_email = form.getvalue('forget_email')
+		otp = form.getvalue('otp')
+		SMTPserver = 'smtp.gmail.com'
+		sender =     ''
+		destination = forget_email
+		USERNAME = ""
+		PASSWORD = ""
+
+		text_subtype = 'html'
+		content="""\
+		Forget Password OTP is : %s
+
+		"""% (otp)
+		subject="%s"% otp
+
+		try:
+			msg = MIMEText(content, text_subtype)
+			msg['Subject']= subject
+			msg['From']   = sender 
+			conn = SMTP(SMTPserver)
+
+			conn.set_debuglevel(False)
+			conn.login(USERNAME, PASSWORD)
+			try:
+				conn.sendmail(sender, destination, msg.as_string())
+				print("1")
+			finally:
+				conn.quit()
+		except Exception:
+			print("0")
+else:
+	print("0")
+	
