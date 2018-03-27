20492efa954ed06d6545d1b30560e5907483590f
Brijeshlakkad <brijeshlakkad22@gmail.com>
2018-03-27 23:06:30 +0530

    added remaining details of candidate in verification page

--- admin_candidate_details.php
+++ admin_candidate_details.php
@@ -16,6 +16,8 @@ if(isset($_POST['query']))
 	$login_name=ucwords($row['Name']);
 	$barV=$row['Progress'];
 	$qualis=explode(",/,",$row['Quali']);
+	$degree=$row['Degree'];
+	$exp_year=$row['Experience'];
 	$course=$row['Course'];
 	$p_year=$row['Passing_year'];
 	$intern=$row['Intern'];
@@ -24,6 +26,9 @@ if(isset($_POST['query']))
 	$postal_add=$row['Postal_Add'];
 	$perm_add=$row['Perm_Add'];
 	$per_pin=$row['Per_pincode'];
+	$country=$row['Country'];
+	$state=$row['State'];
+	$city=$row['City'];
 	$dob= date('d/m/Y', strtotime($row['DOB']));
 	$gender=$row['Gender'];
 	$updated=$row['isUpdated'];
@@ -164,6 +169,10 @@ helper();
 			<div id="show_gra">
 			<table class="myTable">
 							<tr>
+								<td>Degree</td>
+								<td><?php echo $degree; ?></td>
+							</tr>
+							<tr>
 								<td>Course</td>
 								<td><?php echo $course; ?></td>
 							</tr>
@@ -182,6 +191,10 @@ helper();
 							<tr>
 								<td>Internship/Experience</td>
 								<td><?php echo $intern; ?></td>
+							</tr>
+							<tr>
+								<td>Experience Year</td>
+								<td><?php echo $exp_year; ?></td>
 							</tr>
 			</table>
 			<?php 
@@ -207,6 +220,18 @@ helper();
 							<tr>
 								<td>Permanent Address</td>
 								<td><?php echo $perm_add; ?></td>
+							</tr>
+							<tr>
+								<td>Country</td>
+								<td><?php echo $country; ?></td>
+							</tr>
+							<tr>
+								<td>State</td>
+								<td><?php echo $state; ?></td>
+							</tr>
+							<tr>
+								<td>City</td>
+								<td><?php echo $city; ?></td>
 							</tr>
 							<tr>
 								<td>Pincode</td>
