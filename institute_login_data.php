922ecbafbc2cb434d7e7de46689a4bbc3eac49d5
Brijeshlakkad <brijeshlakkad22@gmail.com>
2018-03-31 19:20:47 +0530

    Fixed bug in institute login which was appearing when enter wrong password

--- institute_login_data.php
+++ institute_login_data.php
@@ -1,14 +1,13 @@
 <?php
 include_once("config.php");
 include_once("functions.php");
-check_session();
 $email=protect_anything($_POST['l_email']);
 $password=protect_anything($_POST['l_password']);
 $sql="SELECT * FROM institutes WHERE Email='$email' AND Password='$password'";
 $result=mysqli_query($con,$sql);
 $r=mysqli_num_rows($result);
 $row=mysqli_fetch_assoc($result);
-if($r>0)
+if($r==1)
 {
 	$_SESSION['Userid']=$email;
 	echo "1";
