22aea45b87ae84220ea300de42d88f07186e9da6
Brijeshlakkad <brijeshlakkad22@gmail.com>
2018-04-22 10:48:14 +0530

    to show no found image when rowcount==0

---
+++ no_found.py
@@ -0,0 +1,9 @@
+#!C:\Users\RAJ\AppData\Local\Programs\Python\Python36\python
+import sys
+import os
+def no_found():
+	print("""<div class="row" align="center" style="margin-top: 80px;">
+	<div id="no_found"><img src="Images/not-found2.png" width="100px" alt="no found" /></div>
+	<br/>
+	<div style="color:gray;">No found(0)</div>
+	</div>""")
