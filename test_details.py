ecddbd697ddd17e06d24071e664de3f76a1f2383
Brijeshlakkad <brijeshlakkad22@gmail.com>
2018-04-22 14:50:36 +0530

    added test class to the update_filter_certificates.py

--- test_details.py
+++ test_details.py
@@ -5,16 +5,19 @@ import os
 import MySQLdb
 
 class test:
-	global title,course,postedby,sub_string
+	global divid,title,course,postedby,sub_string,num_que,time
 	def test_details(self,conn,cursor,testid):
 		sql_test="SELECT * FROM Tests where ID='%s'"%(testid)
 		try:
 			cursor.execute(sql_test)
 			result_of_test = cursor.fetchone()
+			self.divid=result_of_test['ID']
 			self.title=result_of_test['Title']
 			self.course=result_of_test['Course']
 			str_sub=result_of_test['Subjects']
 			self.postedby=result_of_test['Postedby']
+			self.num_que=result_of_test['Total_num']
+			self.time=result_of_test['Time']
 			if self.postedby=="-99":
 				self.postedby="CareerHub"
 			subjects=str_sub.split("|")
--- update_filter_certificate.py
+++ update_filter_certificate.py
@@ -5,6 +5,7 @@ import os
 import MySQLdb
 import candidate_details
 import config
+import test_details
 import no_found
 def update_filter_panel(skillarr1,candid):
 	conn,cursor=config.connect_to_database()
@@ -54,40 +55,19 @@ def update_filter_panel(skillarr1,candid):
 			no_found.no_found("Tests(0)")
 		else:
 			for i in arr_id:
-				sql3="SELECT * FROM Tests where ID='%s'"%(i)
-				try:
-					cursor.execute(sql3)
-					row = cursor.fetchone()
-					divid=row['ID']
-					time=row["Time"]
-					datetime=time.strftime('%H : %M')
-					title=row['Title']
-					num=row['Total_num']
-					course=row['Course']
-					str_sub=row['Subjects']
-					subjects=str_sub.split("|")
-					if len(subjects)==1:
-						sub_string=str_sub.strip()
-					else:
-						sub_string="<ol>"
-						for k in subjects:
-							sub_string+="<li>"+k.strip()+"</li>"
-						sub_string+="</ol>"
-					print("""<div id="%s" class='style_prevu_kit test_div' style="padding:10px;"><div class="row"><div class="col-sm-6"><h3>%s</h3></div><div class="col-sm-6"><span class="glyphicon glyphicon-time"></span> Posted on  %s</div></div><hr /><div class="row"><div class="col-sm-6">Total Questions</div><div class="col-sm-6">%s</div></div><div class="row"><div class="col-sm-6">Course</div><div class="col-sm-6">%s</div></div><div class="row"><div class="col-sm-6">Subjects</div><div class="col-sm-6">%s</div></div><div class="row"><div class="col-sm-6"></div><div class="col-sm-6"><form method="post" action="take_test.php"><input type="hidden" name="test_id" value="%s"/><button type="submit" class="btn btn-primary">Apply now!</button></form></div></div></div><hr/><hr/>"""%(divid,title,time,num,course,sub_string,divid))
-				except:
-					conn.rollback()
-					print("-1")
+					obj=test_details.test()
+					obj.test_details(conn,cursor,i)
+					divid=obj.divid
+					title=obj.title
+					course=obj.course
+					postedby=obj.postedby
+					sub_string=obj.sub_string
+					time=obj.time
+					num_que=obj.num_que
+					print("""<div id="%s" class='style_prevu_kit test_div' style="padding:10px;"><div class="row"><div class="col-sm-6"><h3>%s</h3></div><div class="col-sm-6"><span class="glyphicon glyphicon-time"></span> Posted on  %s</div></div><hr /><div class="row"><div class="col-sm-6">Total Questions</div><div class="col-sm-6">%s</div></div><div class="row"><div class="col-sm-6">Course</div><div class="col-sm-6">%s</div></div><div class="row"><div class="col-sm-6">Subjects</div><div class="col-sm-6">%s</div></div><div class="row"><div class="col-sm-6"></div><div class="col-sm-6"><form method="post" action="take_test.php"><input type="hidden" name="test_id" value="%s"/><button type="submit" class="btn btn-primary">Apply now!</button></form></div></div></div><hr/><hr/>"""%(divid,title,time,num_que,course,sub_string,divid))
 	except:
 		conn.rollback()
 		print("-1")
 	conn.close()
 	
-def update_filter_random(candid):
-	conn,cursor=config.connect_to_database()
-	try:
-		obj=candidate_details.candidate()
-		obj.candidate_details(conn,cursor,candid)
-		quali=obj.quali_arr
-		print("dd")
-	except:
-		print("Server is taking load!!")
+
